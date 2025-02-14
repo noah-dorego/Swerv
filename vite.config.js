60e8f99a6ae468e820e2a01d48a2d4feab4b588b
Noah do Rego <ndore051@uottawa.ca>
2025-02-14 13:00:37 -0500

    Test new service rollup in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { resolve } from 'path'
+import path from 'node:path'
 
 export default {
     base: "/swerv/",
     build: {
-        outDir: 'dist'
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                index: path.resolve('index.html'),
+                services: path.resolve('services.html')
+            }
+        }
     },
     server: {
         port: 8080
